b4b535af694b96d62f5944580c90c448b31dcecb
tehitz <santeri.siirila@gmail.com>
2018-11-30 14:14:54 +0200

    Todo class added

---
+++ src/main/java/com/Todolist/Todolist/Controller/TodoController.java
@@ -0,0 +1,11 @@
+package com.Todolist.Todolist.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TodoController {
+
+
+
+}
---
+++ src/main/java/com/Todolist/Todolist/Domain/Todo.java
@@ -0,0 +1,33 @@
+package com.Todolist.Todolist.Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    private Long id;
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Todo(String text, Long id) {
+        this.id = id;
+        this.text = text;
+
+    }
+}
